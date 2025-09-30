8a9f6cedd3b9d4e0a5c527327349f020f3793bdc
Wyren-O <andreybednyakov738@gmail.com>
2025-09-30 20:53:16 +0500

    Update test_crud_api.py

--- test_crud_api.py
+++ test_crud_api.py
@@ -1,46 +1,41 @@
 import pytest
-import requests
 from fastapi.testclient import TestClient
 from app import app
 
 client = TestClient(app)
-BASE_URL = "http://127.0.0.1:8000/users"
 
 @pytest.fixture
 def create_user():
-    """Создаём пользователя перед тестом и удаляем после"""
     payload = {"name": "Rick", "age": 34}
-    response = requests.post(BASE_URL, json=payload)
+    response = client.post("/users", json=payload)
     assert response.status_code == 201
     data = response.json()
     user_id = data["id"]
     yield user_id
     # Очистка после теста
-    requests.delete(f"{BASE_URL}/{user_id}")
+    client.delete(f"/users/{user_id}")
 
 def test_get_user(create_user):
-    """Проверяем, что пользователь создаётся и доступен по GET"""
     user_id = create_user
-    response = requests.get(f"{BASE_URL}/{user_id}")
+    response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
     data = response.json()
     assert data["name"] == "Rick"
     assert data["age"] == 34
 
 def test_update_user(create_user):
-    """Обновляем данные пользователя"""
     user_id = create_user
     new_data = {"name": "Rick Sanchez", "age": 35}
-    response = requests.put(f"{BASE_URL}/{user_id}", json=new_data)
+    response = client.put(f"/users/{user_id}", json=new_data)
     assert response.status_code == 200
     data = response.json()
     assert data["age"] == 35
     assert data["name"] == "Rick Sanchez"
 
 def test_delete_user(create_user):
-    """Удаляем пользователя и проверяем 404 после удаления"""
     user_id = create_user
-    response = requests.delete(f"{BASE_URL}/{user_id}")
+    response = client.delete(f"/users/{user_id}")
     assert response.status_code == 200
-    check = requests.get(f"{BASE_URL}/{user_id}")
+    check = client.get(f"/users/{user_id}")
     assert check.status_code == 404
+
